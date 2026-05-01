ed71fe4de5486bbc39e35a655d9e054e41ac1d5f
Tim Stough <stoughtm@gmail.com>
2026-05-01 11:04:28 -0700

    Added step target setting.

--- src/c/settings.c
+++ src/c/settings.c
@@ -2,13 +2,18 @@
 #include "settings.h"
 
 #define KEY_TICK_PERSISTENCE 0
+#define KEY_STEP_TARGET      1
 
 void settings_load(Settings *s) {
   s->tick_persistence_ms = persist_exists(KEY_TICK_PERSISTENCE)
       ? (uint16_t)(persist_read_int(KEY_TICK_PERSISTENCE) * 1000)
       : 3000;
+  s->step_target = persist_exists(KEY_STEP_TARGET)
+      ? (uint16_t)persist_read_int(KEY_STEP_TARGET)
+      : 10;
 }
 
 void settings_save(const Settings *s) {
   persist_write_int(KEY_TICK_PERSISTENCE, s->tick_persistence_ms / 1000);
+  persist_write_int(KEY_STEP_TARGET, s->step_target);
 }
--- src/c/settings.h
+++ src/c/settings.h
@@ -3,6 +3,7 @@
 
 typedef struct {
   uint16_t tick_persistence_ms;  // 3000–60000 ms, step 1000
+  uint16_t step_target;          // 1–30 (×1000 = daily step goal)
 } Settings;
 
 void settings_load(Settings *s);
